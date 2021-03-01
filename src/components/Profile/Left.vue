f225c5d9748801afb62096f3f7d16ff13c2854bf
minhpg <67231681+minhpg@users.noreply.github.com>
2021-03-01 14:43:38 +0700

    added profile ui

---
+++ src/components/Profile/Left.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-sheet rounded="lg" min-height="268">
+    <v-container>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              <h3>Intro</h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle
+              ><v-icon size="15" color="grey">mdi-home</v-icon
+              ><span class="subtitle-2 ml-1">Lives in ...</span></v-list-item-subtitle
+            >
+            <v-list-item-subtitle
+              ><v-icon size="15" color="grey">mdi-map-marker</v-icon
+              ><span class="subtitle-2 ml-1">From ...</span></v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list >
+      <v-list class="mt-n3" dense>
+        <v-list-item
+          v-for="(link, i) in links"
+          :key="i"
+          class="pl-5"
+        >
+          <v-list-item-content>
+            <v-btn color="grey darken-3"><span class="subtitle-2 normal">{{link.name}}</span></v-btn>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-container>
+  </v-sheet>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: "Edit Details",
+          path: "/",
+          icon: "mdi-home",
+        },
+        {
+          name: "Add Hobbies",
+          path: "/messages",
+          icon: "mdi-message-outline",
+        },
+        {
+          name: "Edit Featured",
+          path: "/profile",
+          icon: "mdi-account",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.normal {
+  text-transform: none;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container>
+    <v-row justify="space-around">
+      <v-card color="" elevation="0" rounded>
+        <v-img
+          height="400px"
+          width="100%"
+          src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+          class="d-flex"
+          gradient="to bottom, rgba(18,18,18,1),rgb(30,30,30,0),rgb(30,30,30,0.1), #1e1e1e"
+        >
+          <div class="outer bottom">
+            <v-card-title class="inner">
+              <v-avatar size="200" class="mb-n2" color="grey">
+                <v-img alt="user" />
+              </v-avatar>
+            </v-card-title>
+          </div>
+        </v-img>
+        <v-row justify="center" dense>
+          <v-card-title>
+            <h2>Username</h2>
+          </v-card-title>
+        </v-row>
+        <v-row justify="center" class="mt-n7">
+          <v-card-title>
+            <h5 class="blue--text">Add Bio</h5>
+          </v-card-title>
+        </v-row>
+        <v-card-action>
+          <v-tabs background-color="transparent">
+            <v-tab><span class="subtitle-1 normal">Posts</span></v-tab>
+            <v-tab><span class="subtitle-1 normal">About</span></v-tab>
+            <v-tab><span class="subtitle-1 normal">Friends</span></v-tab>
+            <v-tab><span class="subtitle-1 normal">Photos</span></v-tab>
+            <v-tab
+              ><span class="subtitle-1 normal">More</span>
+              <v-icon>mdi-menu-down</v-icon></v-tab
+            >
+          </v-tabs>
+        </v-card-action>
+      </v-card>
+    </v-row>
+    <v-row>
+      <v-col col="12" md="12" lg="4">
+        <Left />
+      </v-col>
+      <v-col col="12" md="12" sm="12" lg="8">
+        <v-sheet min-height="7vw"> </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Left from "../components/Profile/Left";
+export default {
+  components: {
+    Left,
+  },
+};
+</script>
+
+<style>
+.bottom {
+  position: absolute;
+  bottom: 0;
+}
+.inner {
+  display: block;
+}
+
+.outer {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.normal {
+  text-transform: none;
+}
+</style>
